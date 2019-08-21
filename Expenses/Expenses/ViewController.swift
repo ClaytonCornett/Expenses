f273ea3411fefab456014c2408bbabf29f24b80c
Clayton Cornett <claytoncornett@mwc-198098.dhcp.missouri.edu>
2019-08-21 11:01:59 -0500

    First commit/added table

---
+++ Expenses/Expenses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Expenses
+//
+//  Created by Clayton Cornett on 8/21/19.
+//  Copyright © 2019 Clayton Cornett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
